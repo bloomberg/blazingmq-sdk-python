ac01847f917d075f4e2c610a506403d9896a3a2e
Patrick M. Niedzielski <pniedzielski@bloomberg.net>
2026-06-22 13:23:12 -0400

    Chore: Add Google Test as dependency
    Signed-off-by: Patrick M. Niedzielski <pniedzielski@bloomberg.net>

--- bin/build-macos-universal.sh
+++ bin/build-macos-universal.sh
@@ -16,6 +16,7 @@ brew install \
     bison \
     flex \
     google-benchmark \
+    googletest \
     cmake
 PREREQUISITES
 
--- bin/build-manylinux.sh
+++ bin/build-manylinux.sh
@@ -46,6 +46,9 @@ source ./bin/clone-dependencies.sh
 if [ ! -d "${DIR_THIRDPARTY}/google-benchmark" ]; then
     git clone --depth 1 https://github.com/google/benchmark.git "${DIR_THIRDPARTY}/google-benchmark"
 fi
+if [ ! -d "${DIR_THIRDPARTY}/googletest" ]; then
+    git clone --depth 1 https://github.com/google/googletest.git "${DIR_THIRDPARTY}/googletest"
+fi
 if [ ! -d "${DIR_THIRDPARTY}/bison" ]; then
     mkdir -p "${DIR_THIRDPARTY}/bison"
     curl https://ftp.gnu.org/gnu/bison/bison-3.8.2.tar.xz | tar -Jx -C "${DIR_THIRDPARTY}/bison" --strip-components 1
@@ -80,6 +83,15 @@ if [ ! -e "${DIR_BUILD}/google-benchmark/.complete" ]; then
     cmake --build "${DIR_BUILD}/google-benchmark" --config Release --target install
     popd
     touch "${DIR_BUILD}/google-benchmark/.complete"
+fi
+
+if [ ! -e "${DIR_BUILD}/googletest/.complete" ]; then
+    pushd "${DIR_THIRDPARTY}/googletest"
+    cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${DIR_INSTALL}" -DCMAKE_INSTALL_LIBDIR="lib64" -S . -B "${DIR_BUILD}/googletest"
+    cmake --build "${DIR_BUILD}/googletest" --config Release -j16
+    cmake --build "${DIR_BUILD}/googletest" --config Release --target install
+    popd
+    touch "${DIR_BUILD}/googletest/.complete"
 fi
 
 # Build and install BDE

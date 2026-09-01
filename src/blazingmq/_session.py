43647689f93d890c9fdce19dea4ff450f2740a19
Patrick M. Niedzielski <pniedzielski@bloomberg.net>
2026-09-01 14:10:25 -0400

    Move `authn_credential_provider` argument to avoid API break
    Signed-off-by: Patrick M. Niedzielski <pniedzielski@bloomberg.net>

--- src/blazingmq/_session.py
+++ src/blazingmq/_session.py
@@ -308,11 +308,6 @@ class SessionOptions:
             healthy, `.HostUnhealthy` and `.HostHealthRestored` events with
             never be emitted, and the *suspends_on_bad_host_health* option of
             `QueueOptions` cannot be used.
-        authn_credential_provider (Optional[`~blazingmq.AuthnCredentialProvider`]):
-            An optional callable that returns authentication credentials as a
-            ``(mechanism, data)`` tuple of ``(str, bytes)``, or ``None`` if no
-            credentials are available.  If not provided, no authentication
-            credentials are sent to the broker.
         num_processing_threads:
             The number of threads for the SDK to use for processing events.
             This defaults to 1.
@@ -342,6 +337,11 @@ class SessionOptions:
             96 bytes long.  This is provided for libraries that are wrapping
             this SDK.  Applications directly using the SDK are encouraged *NOT*
             to set this value.
+        authn_credential_provider (Optional[`~blazingmq.AuthnCredentialProvider`]):
+            An optional callable that returns authentication credentials as a
+            ``(mechanism, data)`` tuple of ``(str, bytes)``, or ``None`` if no
+            credentials are available.  If not provided, no authentication
+            credentials are sent to the broker.
     """
 
     def __init__(
@@ -349,26 +349,26 @@ class SessionOptions:
         message_compression_algorithm: Optional[CompressionAlgorithmType] = None,
         timeouts: Optional[Timeouts] = None,
         host_health_monitor: Union[BasicHealthMonitor, None] = (DefaultMonitor()),
-        authn_credential_provider: Optional[AuthnCredentialProvider] = (
-            DefaultAuthnCredentialProvider()
-        ),
         num_processing_threads: Optional[int] = None,
         blob_buffer_size: Optional[int] = None,
         channel_high_watermark: Optional[int] = None,
         event_queue_watermarks: Optional[tuple[int, int]] = None,
         stats_dump_interval: Optional[float] = None,
         user_agent_prefix: Optional[bytes] = None,
+        authn_credential_provider: Optional[AuthnCredentialProvider] = (
+            DefaultAuthnCredentialProvider()
+        ),
     ) -> None:
         self.message_compression_algorithm = message_compression_algorithm
         self.timeouts = timeouts
         self.host_health_monitor = host_health_monitor
-        self.authn_credential_provider = authn_credential_provider
         self.num_processing_threads = num_processing_threads
         self.blob_buffer_size = blob_buffer_size
         self.channel_high_watermark = channel_high_watermark
         self.event_queue_watermarks = event_queue_watermarks
         self.stats_dump_interval = stats_dump_interval
         self.user_agent_prefix = user_agent_prefix
+        self.authn_credential_provider = authn_credential_provider
 
     def __eq__(self, other: object) -> bool:
         if not isinstance(other, SessionOptions):
@@ -377,13 +377,13 @@ class SessionOptions:
             self.message_compression_algorithm == other.message_compression_algorithm
             and self.timeouts == other.timeouts
             and self.host_health_monitor == other.host_health_monitor
-            and self.authn_credential_provider == other.authn_credential_provider
             and self.num_processing_threads == other.num_processing_threads
             and self.blob_buffer_size == other.blob_buffer_size
             and self.channel_high_watermark == other.channel_high_watermark
             and self.event_queue_watermarks == other.event_queue_watermarks
             and self.stats_dump_interval == other.stats_dump_interval
             and self.user_agent_prefix == other.user_agent_prefix
+            and self.authn_credential_provider == other.authn_credential_provider
         )
 
     def __ne__(self, other: object) -> bool:
@@ -394,13 +394,13 @@ class SessionOptions:
             "message_compression_algorithm",
             "timeouts",
             "host_health_monitor",
-            "authn_credential_provider",
             "num_processing_threads",
             "blob_buffer_size",
             "channel_high_watermark",
             "event_queue_watermarks",
             "stats_dump_interval",
             "user_agent_prefix",
+            "authn_credential_provider",
         )
 
         params = []
@@ -443,11 +443,6 @@ class Session:
             `.HostHealthRestored` events will never be emitted, and the
             *suspends_on_bad_host_health* option of `QueueOptions` cannot be
             used.
-        authn_credential_provider (Optional[`~blazingmq.AuthnCredentialProvider`]):
-            an optional callable that returns authentication credentials as a
-            ``(mechanism, data)`` tuple of ``(str, bytes)``, or ``None`` if no
-            credentials are available.  If not provided, no authentication
-            credentials are sent to the broker.
         num_processing_threads: The number of threads for the SDK to use for
             processing events.  This defaults to 1.
         blob_buffer_size: The size (in bytes) of the blob buffers to use.  This
@@ -473,6 +468,11 @@ class Session:
             must be at most 96 bytes long.  This is provided for libraries
             that are wrapping this SDK.  Applications directly using the SDK
             are encouraged *NOT* to set this value.
+        authn_credential_provider (Optional[`~blazingmq.AuthnCredentialProvider`]):
+            an optional callable that returns authentication credentials as a
+            ``(mechanism, data)`` tuple of ``(str, bytes)``, or ``None`` if no
+            credentials are available.  If not provided, no authentication
+            credentials are sent to the broker.
 
     Raises:
         `~blazingmq.Error`: If the session start request was not successful.
@@ -492,15 +492,15 @@ class Session:
         ),
         timeout: Union[Timeouts, float] = DEFAULT_TIMEOUT,
         host_health_monitor: Union[BasicHealthMonitor, None] = (DefaultMonitor()),
-        authn_credential_provider: Optional[AuthnCredentialProvider] = (
-            DefaultAuthnCredentialProvider()
-        ),
         num_processing_threads: Optional[int] = None,
         blob_buffer_size: Optional[int] = None,
         channel_high_watermark: Optional[int] = None,
         event_queue_watermarks: Optional[tuple[int, int]] = None,
         stats_dump_interval: Optional[float] = None,
         user_agent_prefix: Optional[bytes] = None,
+        authn_credential_provider: Optional[AuthnCredentialProvider] = (
+            DefaultAuthnCredentialProvider()
+        ),
     ) -> None:
         if host_health_monitor is not None:
             if not isinstance(host_health_monitor, BasicHealthMonitor):
@@ -602,13 +602,13 @@ class Session:
             message_compression_algorithm=message_compression_algorithm,
             timeout=timeout,
             host_health_monitor=session_options.host_health_monitor,
-            authn_credential_provider=session_options.authn_credential_provider,
             num_processing_threads=session_options.num_processing_threads,
             blob_buffer_size=session_options.blob_buffer_size,
             channel_high_watermark=session_options.channel_high_watermark,
             event_queue_watermarks=session_options.event_queue_watermarks,
             stats_dump_interval=session_options.stats_dump_interval,
             user_agent_prefix=session_options.user_agent_prefix,
+            authn_credential_provider=session_options.authn_credential_provider,
         )
 
     def open_queue(
